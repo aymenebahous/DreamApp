82d247d922cbc73e033eadf90d6b27ff45cce92b
Aymene <aymanebahous04@gmail.com>
2023-07-15 03:22:35 +0200

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/server/server.component.css 	new file:   src/app/server/server.component.html 	new file:   src/app/server/server.component.ts 	new file:   src/app/servers/servers.component.css 	new file:   src/app/servers/servers.component.html 	new file:   src/app/servers/servers.component.ts 	new file:   src/app/success-alert/success-alert.component.css 	new file:   src/app/success-alert/success-alert.component.html 	new file:   src/app/success-alert/success-alert.component.ts 	new file:   src/app/warning-alert/warning-alert.component.css 	new file:   src/app/warning-alert/warning-alert.component.html 	new file:   src/app/warning-alert/warning-alert.component.ts 	modified:   src/index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
